af66eed4f4552021e4782fb2af79241c17f3641d
Ronny <mwithiamoki@gmail.com>
2020-10-12 16:24:33 +0300

    add class and loop on search

--- photos/models.py
+++ photos/models.py
@@ -3,10 +3,20 @@ from cloudinary.models import CloudinaryField
 class Image(models.Model):
     title = models.CharField(max_length=100)
     image = CloudinaryField('image')    
-    
+    description = models.CharField(max_length =100)
+    categorytags = models.ManyToManyField(Category)
+    locationtags = models.ManyToManyField(Location)
+    date = models.DateTimeField(auto_now_add=True)
+
+
 
     def __str__(self):
         return self.title
 
-    class Meta:
+    class Meta:                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                                 
         verbose_name_plural='images'
+
+    @classmethod
+    def search_by_title(cls,search_term):
+        photos = cls.objects.filter(title__icontains=search_term)
+        return photos                                                                   
--- photos/views.py
+++ photos/views.py
@@ -11,10 +11,10 @@ def welcome(request):
 def search(request):
     if "term" in request.GET and request.GET["term"]:
         term = request.GET.get("term")
-        photos = Image.search_image(term)
-        if photos != "No images found":
+        image = Image.search_image(term)
+        if image != "No images found":
             message = "Photos of '" + term + "'"
-            return render(request, "search.html", {"images":photos,"message":message,"title":term.capitalize()})
+            return render(request, "search.html", {"images":images,"message":message,"title":term.capitalize()})
         else:
             message = "No images found"
             return render(request, "search.html", {"images":[],"message":message,"title":term.capitalize()})
@@ -25,4 +25,7 @@ def photos(request,photos_id):
         photos = Photos.objects.get(id = photos_id)
     except DoesNotExist:
         raise Http404()
-    return render(request,"gallery/photos.html", {"photos":photos})    
+    return render(request,"photos/photos.html", {"photos":photos})    
+
+
+ def category(request)    
