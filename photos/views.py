c5a108f85372104e42bb79fd393b50b38bab1331
Ronny <mwithiamoki@gmail.com>
2020-10-12 04:56:23 +0300

    add defined search

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-fr.om django.http import HttpResponse
+from django.http import HttpResponse, Http404
 import datetime as dt
 from .models import Image
 def welcome(request):
@@ -7,6 +7,15 @@ def welcome(request):
     Image = Image.objects.all()
     return render(request,'photos/home.html', {'title': title, 'images':images})
 
-def dynamic_urls(request, query):
-    q_object = query
-    return HttpResponse(q_object)
+
+def search(request):
+    if "term" in request.GET and request.GET["term"]:
+        term = request.GET.get("term")
+        photos = Image.search_image(term)
+        if photos != "No images found":
+            message = "Photos of '" + term + "'"
+            return render(request, "search.html", {"images":photos,"message":message,"title":term.capitalize()})
+        else:
+            message = "No images found"
+            return render(request, "search.html", {"images":[],"message":message,"title":term.capitalize()})
+    
