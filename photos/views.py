1ef93270956f2c70b750face99a8043afe16fc06
Ronny <mwithiamoki@gmail.com>
2020-10-12 04:21:32 +0300

    add def dynamic

--- photos/views.py
+++ photos/views.py
@@ -1,5 +1,12 @@
 from django.shortcuts import render
-
+fr.om django.http import HttpResponse
+import datetime as dt
+from .models import Image
 def welcome(request):
     title = 'welcome'
-    return render(request,'photos/home.html', {'title': title})
+    Image = Image.object.all()
+    return render(request,'photos/home.html', {'title': title, 'images':images})
+
+def dynamic_urls(request, query):
+    q_object = query
+    return HttpResponse(q_object)
