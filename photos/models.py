94a729d724ba4ad627ab900a1116137737f911ba
Ronny <mwithiamoki@gmail.com>
2020-10-12 04:06:12 +0300

    add class image defined and import cloudinaryfield

--- photos/models.py
+++ photos/models.py
@@ -1,5 +1,11 @@
 from django.db import models
-from cloudinary import CloudinaryImage
+from cloudinary.models import Cloudinaryfield
 class Image(models.Model):
     title = models.CharField(max_length=100)
-    image = CloudinaryImage('image')
+    image = Cloudinaryfield('image')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name_plural='images'
